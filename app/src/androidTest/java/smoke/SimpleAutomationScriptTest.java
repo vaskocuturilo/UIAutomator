fbe0e048df02d6595a0a3f4b9e1cfabfc0432405
asm@dostavista.ru <wolfstonik>
2021-04-17 08:29:27 +0300

    dv_00013: Fix SimpleAutomationScriptTest and added check all elements on the RegisterScreen.

--- app/src/androidTest/java/smoke/SimpleAutomationScriptTest.java
+++ app/src/androidTest/java/smoke/SimpleAutomationScriptTest.java
@@ -4,14 +4,17 @@ import org.junit.Test;
 
 import base.BaseClass;
 import screens.LoginScreen;
+import screens.RegisterScreen;
 
 public class SimpleAutomationScriptTest extends BaseClass {
     /**
-     * This automation script check that all elements is present on the login page.
+     * This automation script check that all elements is present on the login and register pages.
      */
     @Test
     public void simpleAutomationScriptTest() {
         LoginScreen loginScreen = new LoginScreen();
+        RegisterScreen registerScreen = new RegisterScreen();
         loginScreen.checkThatAllElementsIsAvailable();
+        registerScreen.checkThatAllElementsIsAvailable();
     }
 }
